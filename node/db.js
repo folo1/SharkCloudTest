183aad75ce924d448b558db816a7bc84febbcd86
Olivia Folsom <86935346+folo1@users.noreply.github.com>
2025-04-03 23:53:02 -0400

    Update db.js

--- node/db.js
+++ node/db.js
@@ -4,7 +4,8 @@ const MONGO_USERNAME = 'sammy';
 const MONGO_PASSWORD = 'your_password';
 const MONGO_HOSTNAME = 'mongo';
 const MONGO_PORT = '27017';
-const MONGO_DB = 'sharkinfo'
+const MONGO_DB = 'sharkinfo';
 
 const url = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}`;
+
 mongoose.connect(url, {useNewUrlParser: true});
